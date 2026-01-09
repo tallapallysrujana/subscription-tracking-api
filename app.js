eed6531ea38eda99c915ecaf23ccf64c31d61ccf
tallapallysrujana <tallapallysrujana2004@gmail.com>
2026-01-09 17:33:28 +0530

    Setup basic Express server

---
+++ app.js
@@ -0,0 +1,9 @@
+import express from "express";
+const app=express();
+app.get("/", (req, res) => {
+    res.send("Welcome to the Subscription Tracker API!");
+});
+app.listen(3000,()=>{
+    console.log("Subscription Tracker API is running on http://localhost:3000");
+});
+export default app;
