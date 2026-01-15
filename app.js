92a1bef329ab63d8e654777217baf4af5990da86
tallapallysrujana <tallapallysrujana2004@gmail.com>
2026-01-15 20:10:48 +0530

    error handler middleware for errors

--- app.js
+++ app.js
@@ -6,13 +6,17 @@ import userRouter from "./routes/user.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import connectToDatabase from './database/mongodb.js';
+import errorMiddleware from "./middlewares/error.middleware.js";
 
 const app=express();
+
+
+
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/subscriptions', subscriptionRouter);
-
+app.use(errorMiddleware);
 
 app.get("/", (req, res) => {
     res.send("Welcome to the Subscription Tracker API!");
