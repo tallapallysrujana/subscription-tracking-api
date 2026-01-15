35f83ed3acfd6dd872d94aa67c3a52340e8f399c
tallapallysrujana <tallapallysrujana2004@gmail.com>
2026-01-15 20:15:41 +0530

    express buitin middleware

--- app.js
+++ app.js
@@ -7,10 +7,16 @@ import subscriptionRouter from "./routes/subscription.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import connectToDatabase from './database/mongodb.js';
 import errorMiddleware from "./middlewares/error.middleware.js";
+import cookieParser from "cookie-parser";
+
+
+
 
 const app=express();
 
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 
 app.use('/api/v1/auth', authRouter);
