fe133871c4d31bbe0a5d7515cdf47baf2a2df041
tallapallysrujana <tallapallysrujana2004@gmail.com>
2026-01-25 11:43:56 +0530

    testing get all users request

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,12 +1,11 @@
-import {Router} from 'express';
+import { Router } from 'express';
+import { getUser, getUsers } from '../controllers/user.controller.js'
 
+const userRouter = Router();
 
+userRouter.get('/', getUsers);
 
-const userRouter= Router();
-// GET /users ->get all users
-//GET /users/:id -> get users by id
-userRouter.get('/', (req, res) =>res.send( {title:'Get all Users'}));
-userRouter.get('/:id', (req, res) =>res.send( {title:`Get User details`} ));
+userRouter.get('/:id', getUser);
 userRouter.get('/', (req, res) =>res.send({title:'Create a new user'}));
 userRouter.put('/:id', (req, res) =>res.send( {title:`Update User details`} ));
 userRouter.delete('/:id', (req, res) =>res.send( {title:`Delete User`} ));
