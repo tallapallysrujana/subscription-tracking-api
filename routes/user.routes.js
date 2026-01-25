0b0069777614ccb4cfdda2a84866ddaaa83add06
tallapallysrujana <tallapallysrujana2004@gmail.com>
2026-01-25 12:35:20 +0530

    protecting user details from unauthorised use

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,12 +1,18 @@
 import { Router } from 'express';
+
+import authorize from '../middlewares/auth.middleware.js'
 import { getUser, getUsers } from '../controllers/user.controller.js'
 
 const userRouter = Router();
 
 userRouter.get('/', getUsers);
 
-userRouter.get('/:id', getUser);
-userRouter.get('/', (req, res) =>res.send({title:'Create a new user'}));
-userRouter.put('/:id', (req, res) =>res.send( {title:`Update User details`} ));
-userRouter.delete('/:id', (req, res) =>res.send( {title:`Delete User`} ));
+userRouter.get('/:id', authorize, getUser);
+
+userRouter.post('/', (req, res) => res.send({ title: 'CREATE new user' }));
+
+userRouter.put('/:id', (req, res) => res.send({ title: 'UPDATE user' }));
+
+userRouter.delete('/:id', (req, res) => res.send({ title: 'DELETE user' }));
+
 export default userRouter;
